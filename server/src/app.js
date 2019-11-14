df42b081ba43e64d0692196ba7a2d24a28506c38
Bradford M Basinger <rameseslabs@gmail.com>
2019-11-13 22:52:35 -0500

    Integrated front and back end frameworks with register component

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => res.send('Hello World!'))
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello! ${req.body.email} has been registered.`
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
